5fd90eac89d7211bb048d6df0f79f48ed74542db
hskeays <hskeays@gmail.com>
2023-08-18 17:36:35 -0400

    Feat: add comments

--- index.js
+++ index.js
@@ -1,49 +1,62 @@
+// Creates array with valid choices
 const options = ["rock", "paper", "scissors"];
 
+// Creates function to create a random choice for computer
 function getComputerChoice() {
     const randomChoice = options[Math.floor(Math.random() * options.length)];
     return randomChoice;
 }
 
+// Creates function to get player choice, asks with prompt
+// Creates loop that repeats prompt if user input != array choice
 function getPlayerChoice() {
-    let input = false;
-    // loops until input = true
-    while (input == false) {
+    let correctInput = false;
+    while (correctInput == false) {
+        // Saves prompt input to CHOICE variable
         const choice = prompt("Rock, Paper, Scissors?");
+        // Continues loop if choice is null
         if (choice == null) {
             continue;
-            // continuesloop
         }
+        //changes prompt input to lowercase, saved in new variable
         const choiceInLower = choice.toLowerCase();
+        // Stops prompt loop if prompt input is == array choice
+        // Returns prompt input in lowercase
         if (options.includes(choiceInLower)) {
             input = true;
             return choiceInLower;
+
         }
     }
 }
 
+// Creates function to check for a winner
+// Returns the winner name, or tie
 function checkWinner(compChoice, playerChoice) {
-    if (playerChoice === compChoice) {
+    if (playerChoice == compChoice) {
         return "Tie"
-    } 
+    }
     else if (
-        (playerChoice === "rock" && compChoice === "scissors") ||
-        (playerChoice === "paper" && compChoice === "rock") ||
-        (playerChoice === "scissors" && compChoice === "paper")
-     ){
+        (playerChoice == "rock" && compChoice === "scissors") ||
+        (playerChoice == "paper" && compChoice === "rock") ||
+        (playerChoice == "scissors" && compChoice === "paper")
+    ) {
         return "Player";
-     }
-     else {
+    }
+    else {
         return "Computer";
     }
 }
 
+// Creates function that plays a single round
+// Saves checkWinner() result in RESULT
+// Returns messages of results
 function playRound(compChoice, playerChoice) {
     const result = checkWinner(compChoice, playerChoice);
-    if (result ===  "Tie") {
+    if (result == "Tie") {
         return "It's a tie!"
     }
-    else if (result === "Player") {
+    else if (result == "Player") {
         return `You Win! ${playerChoice} beats ${compChoice}!`
     }
     else {
@@ -51,9 +64,12 @@ function playRound(compChoice, playerChoice) {
     }
 }
 
+// Creates function to play 5 rounds
 function game() {
     console.log("Are you ready? Let's play!")
-    
+
+    // Creates loop to replay round 5 times
+    // Logs results from playRound() function
     for (let i = 0; i < 5; i++) {
         const playerChoice = getPlayerChoice();
         const compChoice = getComputerChoice();
@@ -63,4 +79,4 @@ function game() {
     console.log("Game Over!");
 }
 
-game()
+// game()
