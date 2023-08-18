d134835d88f6ab1c1ef74adf5b9d8d420308bf6d
hskeays <hskeays@gmail.com>
2023-08-18 16:56:54 -0400

    Fix: rework to allow check winner, scoring

--- index.js
+++ index.js
@@ -1,42 +1,66 @@
-// Generates random string choice
+const options = ["rock", "paper", "scissors"];
+
 function getComputerChoice() {
-    const choice = ["rock", "paper", "scissors"];
-    const randomChoice = choice[Math.floor(Math.random() * choice.length)];
+    const randomChoice = options[Math.floor(Math.random() * options.length)];
     return randomChoice;
 }
 
-
-// Creates window prompt for user input
-const userInput = window.prompt("Rock, Paper, or Scissors! :")
-
-if (userInput.toLowerCase() === "rock" || "paper" || "scissors") {
-    console.log(playRound(getComputerChoice(), userInput.toLowerCase()));
-}
-
-function playRound(compChoice, playerChoice) {
-    if (playerChoice.toLowerCase() === "rock" && compChoice === "scissors") {
-        return "You Win! Rock beats scissors";
-    } else if (playerChoice.toLowerCase() === "rock" && compChoice === "paper") {
-        return "You Lose! Paper beats rock";
-    } else if (playerChoice.toLowerCase() === "paper" && compChoice === "rock") {
-        return "You Win! Paper beats rock";
-    } else if (playerChoice.toLowerCase() === "paper" && compChoice === "scissors") {
-        return "You Lose! Scissors beats paper";
-    } else if (playerChoice.toLowerCase() === "scissors" && compChoice === "paper") {
-        return "You Win! Scissors beats paper";
-    } else if (playerChoice.toLowerCase() === "scissors" && compChoice === "rock") {
-        return "You Lose! Rock beats Scissors";
-    } else {
-        return playRound(getComputerChoice(), userInput.toLowerCase());
+function getPlayerChoice() {
+    let input = false;
+    // loops until input = true
+    while (input == false) {
+        const choice = prompt("Rock, Paper, Scissors?");
+        if (choice == null) {
+            continue;
+            // continuesloop
+        }
+        const choiceInLower = choice.toLowerCase();
+        if (options.includes(choiceInLower)) {
+            input = true;
+            return choiceInLower;
+        }
     }
 }
 
-// in progress
+function checkWinner(compChoice, playerChoice) {
+    if (playerChoice === compChoice) {
+        return "Tie"
+    } 
+    else if (
+        (playerChoice === "rock" && compChoice === "scissors") ||
+        (playerChoice === "paper" && compChoice === "rock") ||
+        (playerChoice === "scissors" && compChoice === "paper")
+     ){
+        return "Player";
+     }
+     else {
+        return "Computer";
+    }
+}
 
-// function game() {
-//     console.log(playRound(getComputerChoice(), userInput.toLowerCase()));
-//     console.log(playRound(getComputerChoice(), userInput.toLowerCase()));
-//     console.log(playRound(getComputerChoice(), userInput.toLowerCase()));
-//     console.log(playRound(getComputerChoice(), userInput.toLowerCase()));
-//     console.log(playRound(getComputerChoice(), userInput.toLowerCase()));
-// }
+function playRound(compChoice, playerChoice) {
+    const result = checkWinner(compChoice, playerChoice);
+    if (result ===  "Tie") {
+        return "It's a tie!"
+    }
+    else if (result === "Player") {
+        return `You Win! ${playerChoice} beats ${compChoice}!`
+    }
+    else {
+        return `You Lose! ${compChoice} beats ${playerChoice}!`
+    }
+}
+
+function game() {
+    console.log("Are you ready? Let's play!")
+    
+    for (let i = 0; i < 5; i++) {
+        const playerChoice = getPlayerChoice();
+        const compChoice = getComputerChoice();
+        console.log(playRound(compChoice, playerChoice));
+    }
+
+    console.log("Game Over!");
+}
+
+game()
