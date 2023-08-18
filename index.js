ba0643a79b0e09413038dd541d57a87792bc18a5
hskeays <hskeays@gmail.com>
2023-08-17 20:54:47 -0400

    Fix: disallow draws, add window prompt

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
+// Generates random string choice
 function getComputerChoice() {
     const choice = ["rock", "paper", "scissors"];
     const randomChoice = choice[Math.floor(Math.random() * choice.length)];
     return randomChoice;
 }
 
-function play(compChoice, playerChoice) {
 
-    if (playerChoice.toLowerCase() === "rock" && getComputerChoice() === "scissors") {
+// Creates window prompt for user input
+const userInput = window.prompt("Rock, Paper, or Scissors! :")
+
+if (userInput.toLowerCase() === "rock" || "paper" || "scissors") {
+    console.log(playRound(getComputerChoice(), userInput.toLowerCase()));
+}
+
+function playRound(compChoice, playerChoice) {
+    if (playerChoice.toLowerCase() === "rock" && compChoice === "scissors") {
         return "You Win! Rock beats scissors";
-    } else if (playerChoice.toLowerCase() === "rock" && getComputerChoice() === "paper") {
+    } else if (playerChoice.toLowerCase() === "rock" && compChoice === "paper") {
         return "You Lose! Paper beats rock";
-    } else if (playerChoice.toLowerCase() === "paper" && getComputerChoice() === "rock") {
+    } else if (playerChoice.toLowerCase() === "paper" && compChoice === "rock") {
         return "You Win! Paper beats rock";
-    } else if (playerChoice.toLowerCase() === "paper" && getComputerChoice() === "scissors") {
+    } else if (playerChoice.toLowerCase() === "paper" && compChoice === "scissors") {
         return "You Lose! Scissors beats paper";
-    } else if (playerChoice.toLowerCase() === "scissors" && getComputerChoice() === "paper") {
+    } else if (playerChoice.toLowerCase() === "scissors" && compChoice === "paper") {
         return "You Win! Scissors beats paper";
-    } else if (playerChoice.toLowerCase() === "scissors" && getComputerChoice() === "rock") {
+    } else if (playerChoice.toLowerCase() === "scissors" && compChoice === "rock") {
         return "You Lose! Rock beats Scissors";
     } else {
-        return "Same choice! Try Again!";
+        return playRound(getComputerChoice(), userInput.toLowerCase());
     }
 }
 
-function game() {
+// in progress
 
-}
+// function game() {
+//     console.log(playRound(getComputerChoice(), userInput.toLowerCase()));
+//     console.log(playRound(getComputerChoice(), userInput.toLowerCase()));
+//     console.log(playRound(getComputerChoice(), userInput.toLowerCase()));
+//     console.log(playRound(getComputerChoice(), userInput.toLowerCase()));
+//     console.log(playRound(getComputerChoice(), userInput.toLowerCase()));
+// }
