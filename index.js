8b155f1fe1545ab739e6ecf2310a1d8f217e5646
hskeays <hskeays@gmail.com>
2023-08-25 18:13:42 -0400

    Remove comments

--- index.js
+++ index.js
@@ -1,37 +1,26 @@
-// Creates array with valid choices
 const options = ["rock", "paper", "scissors"];
 
-// Creates function to get random choice for computer
 function getComputerChoice() {
     const randomChoice = options[Math.floor(Math.random() * options.length)];
     return randomChoice;
 }
 
-// Creates function to get player choice, asks with prompt window
-// Creates loop that repeats prompt if user input != array choice
 function getPlayerChoice() {
     let correctInput = false;
     while (correctInput == false) {
-        // Saves prompt input to CHOICE variable
         const choice = prompt("Rock, Paper, Scissors?");
-        // Continues loop if choice is null
         if (choice == null) {
             continue;
         }
-        //changes prompt input to lowercase, saved in new variable
         const choiceInLower = choice.toLowerCase();
-        // Stops prompt loop if prompt input is == array choice
-        // Returns prompt input in lowercase
         if (options.includes(choiceInLower)) {
-            input = true;
+            correctInput = true;
             return choiceInLower;
 
         }
     }
 }
 
-// Creates function to check for a winner
-// Returns the winner name, or tie
 function checkWinner(compChoice, playerChoice) {
     if (playerChoice == compChoice) {
         return "Tie";
@@ -48,9 +37,6 @@ function checkWinner(compChoice, playerChoice) {
     }
 }
 
-// Creates function that plays a single round
-// Saves checkWinner() result in RESULT
-// Returns messages of results
 function playRound(compChoice, playerChoice) {
     const result = checkWinner(compChoice, playerChoice);
     if (result == "Tie") {
@@ -64,20 +50,14 @@ function playRound(compChoice, playerChoice) {
     }
 }
 
-// Creates function to play 5 rounds
 function game() {
-    // Creates scores for both players, starting at 0
     let playerScore = 0;
     let compScore = 0;
     console.log("Are you ready? Let's play!");
-    // Creates loop to replay round 5 times
-    // Logs results from playRound() function
     for (let i = 0; i < 5; i++) {
         const compChoice = getComputerChoice();
         const playerChoice = getPlayerChoice();
         console.log(playRound(compChoice, playerChoice));
-        // Increments player score if player wins
-        // Increments computer score if computer wins
         if (checkWinner(compChoice, playerChoice) == "Player") {
             playerScore++;
         }
@@ -85,9 +65,8 @@ function game() {
             compScore++;
         }
     }
-    // Logs score of both players
+  
     console.log(`Player score: ${playerScore} | Computer score: ${compScore}`);
-    // Compares player and computer score and determines winner
     if (playerScore > compScore) {
         console.log("YOU WIN!");
     }
@@ -100,4 +79,4 @@ function game() {
     console.log("Game over! Thanks for playing!");
 }
 
-game();
+// game();
